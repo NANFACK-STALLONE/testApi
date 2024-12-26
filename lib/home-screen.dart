d8b846db63c57ea5d556517b418ef83ef0638978
NANFACK-STALLONE <stallonenanfack45@gmail.com>
2024-12-26 18:26:26 +0100

    test api

---
+++ lib/home-screen.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_api1/Models/PostModels.dart';
+import 'package:http/http.dart' as http;
+
+class homeScreen extends StatefulWidget {
+  const homeScreen({super.key});
+
+  @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  List<PostModels> postList = [];
+  Future<List<PostModels>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      postList.clear();
+      for (Map i in data) {
+        postList.add(PostModels.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("api test"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getPostApi(),
+                builder: (context, snaphot) {
+                  if (!snaphot.hasData) {
+                    return Text("Loading destruction totale");
+                  } else {
+                    return ListView.builder(
+                        itemCount: postList.length,
+                        itemBuilder: (Context, index) {
+                          return Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text("Title",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),
+                                  Text(postList[index].title.toString()),
+                                  SizedBox(height: 5,),
+                                  Text("Description",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),
+                                  Text(postList[index].body.toString())
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                  }
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
